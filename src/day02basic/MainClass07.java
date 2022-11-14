2f429de0a74e813ec68bb698bd9728c6e1f8427b
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 21:48:01 +0900

    asd

---
+++ src/day02basic/MainClass07.java
@@ -0,0 +1,19 @@
+package day02basic;
+/*
+ * [[참조형 데이터(레퍼런스)]]
+ * 
+ * String 형 (문자열)
+ * -
+ * 	String;
+ * - 문자열을 저장할 수 있다.
+ * 
+ * 
+ * 
+ */
+public class MainClass07 {
+	public static void main(String[] args) {
+		// 변수 선언과 동시에 값 대입하기.
+		String str = "안녕하세요";
+		System.out.println("str : " + str);
+	}
+}
